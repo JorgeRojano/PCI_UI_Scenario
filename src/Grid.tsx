c749fc0477b1ea2e1f8f81ed29384c60cbae33de
JorgeRojano <jorgerojano@Admins-MBP>
2023-07-20 04:07:31 -0600

    Task#3

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -4,22 +4,44 @@ import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+const dateFormatter = (params: { value: string | number | Date; }) => {
+  const dateObj = new Date(params.value);
+  const day = String(dateObj.getDate()).padStart(2, '0');
+  const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+  const year = String(dateObj.getFullYear()).slice(-2);
+
+  return `${day}/${month}/${year}`;
+};
+
 const defaultColDef = {
   sortable: true,
-  filter: 'agTextColumnFilter',
+  filter: "agTextColumnFilter",
 };
 
 const columnDefs: ColDef[] = [
   { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date", filter: 'agDateColumnFilter' },
-  { field: "h_mag", headerName: "H (mag)", filter: 'agNumberColumnFilter' },
-  { field: "moid_au", headerName: "MOID (au)", filter: 'agNumberColumnFilter' },
-  { field: "q_au_1", headerName: "q (au)", filter: 'agNumberColumnFilter' },
-  { field: "q_au_2", headerName: "Q (au)", filter: 'agNumberColumnFilter' },
-  { field: "period_yr", headerName: "Period (yr)", filter: 'agNumberColumnFilter' },
-  { field: "i_deg", headerName: "Inclination (deg)", filter: 'agNumberColumnFilter' },
+  {
+    field: "discovery_date",
+    headerName: "Discovery Date",
+    filter: "agDateColumnFilter",
+    valueFormatter: dateFormatter,
+  },
+  { field: "h_mag", headerName: "H (mag)", filter: "agNumberColumnFilter" },
+  { field: "moid_au", headerName: "MOID (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_1", headerName: "q (au)", filter: "agNumberColumnFilter" },
+  { field: "q_au_2", headerName: "Q (au)", filter: "agNumberColumnFilter" },
+  {
+    field: "period_yr",
+    headerName: "Period (yr)",
+    filter: "agNumberColumnFilter",
+  },
+  {
+    field: "i_deg",
+    headerName: "Inclination (deg)",
+    filter: "agNumberColumnFilter",
+  },
   { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
+  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true },
 ];
 
 const NeoGrid = (): JSX.Element => {
