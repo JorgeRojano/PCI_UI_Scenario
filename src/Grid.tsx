73a877bc17a86358ee4545b2b8e5f6889e1ceb18
JorgeRojano <jorgerojano@Admins-MBP>
2023-07-20 05:10:28 -0600

    Task#6

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -3,27 +3,28 @@ import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { useRef } from "react";
 
-const dateFormatter = (params: { value: string | number | Date; }) => {
+const dateFormatter = (params: { value: string | number | Date }) => {
   const dateObj = new Date(params.value);
-  const day = String(dateObj.getDate()).padStart(2, '0');
-  const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+  const day = String(dateObj.getDate()).padStart(2, "0");
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
   const year = String(dateObj.getFullYear()).slice(-2);
 
   return `${day}/${month}/${year}`;
 };
 
-const hazardousAnswerFormatter = (params: { value: string; }) => {
-  if (params.value === 'Y') {
-    return 'Yes';
-  } else if (params.value === 'N') {
-    return 'No';
-  } else if (params.value === 'n/a') {
-    return '';
+const hazardousAnswerFormatter = (params: { value: string }) => {
+  if (params.value === "Y") {
+    return "Yes";
+  } else if (params.value === "N") {
+    return "No";
+  } else if (params.value === "n/a") {
+    return "";
   } else {
     return params.value;
   }
-}
+};
 
 const defaultColDef = {
   sortable: true,
@@ -61,16 +62,40 @@ const columnDefs: ColDef[] = [
 ];
 
 const NeoGrid = (): JSX.Element => {
+  const gridRef = useRef<AgGridReact>(null);
+
+  const handleClear = () => {
+    console.log("AQUI")
+      gridRef.current?.api.setFilterModel(null)
+      gridRef.current?.columnApi.applyColumnState({
+        defaultState: { sort: null },
+      });
+  }
+
   return (
-    <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
-      <AgGridReact
-        rowData={data}
-        columnDefs={columnDefs}
-        defaultColDef={defaultColDef}
-        rowGroupPanelShow={'always'}
-        /* enableRangeSelection is only available in enterprise version https://www.ag-grid.com/react-data-grid/range-selection/#copy-range-down */
-      />
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "15px",
+          alignItems: "center",
+        }}
+      >
+        <h1>Near-Earth Object Overview</h1>
+        <button onClick={handleClear}>Clear Filters and Sorters</button>
+      </div>
+      <div className="ag-theme-alpine" style={{ height: 900, width: 1920 }}>
+        <AgGridReact
+          ref={gridRef}
+          rowData={data}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          rowGroupPanelShow={"always"}
+          /* enableRangeSelection is only available in enterprise version https://www.ag-grid.com/react-data-grid/range-selection/#copy-range-down */
+        />
+      </div>
+    </>
   );
 };
 
